78cbfaad97906304ae88e50b5594886d4d172411
JoseAl05 <jparancibialinker@gmail.com>
2023-05-20 22:16:10 -0400

    create a hook to handle images

---
+++ app/hooks/useImage.ts
@@ -0,0 +1,25 @@
+import useSWR from 'swr';
+import axios from 'axios';
+import { ImageID } from '../types';
+
+interface IUseImage {
+  title?: string;
+}
+
+const useImage = ({ title }: IUseImage) => {
+
+  const formattedTitle = title?.split(' ').join('%20');
+
+  const wikiEndPoint = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages&indexpageids=1&titles=${formattedTitle}&piprop=original`;
+
+  const fetcher = async (url) => await axios.get(url).then((res) => res.data);
+  const { data, error, isLoading } = useSWR(wikiEndPoint, fetcher);
+
+  return {
+    imageData: data?.query as ImageID,
+    error,
+    isLoading,
+  };
+};
+
+export default useImage;
