931c1e575f93a29d3dd755d4c4b0597faa9bbaaf
JoseAl05 <jparancibialinker@gmail.com>
2023-05-23 22:34:04 -0400

    create circuit response type

---
+++ app/types/CircuitTypes.ts
@@ -0,0 +1,11 @@
+export type CircuitResponse = {
+    circuitId:number;
+    circuitRef:string;
+    name:string;
+    country:string;
+    location:string;
+    alt:number;
+    lat:number;
+    lng:number;
+    url:string;
+}
