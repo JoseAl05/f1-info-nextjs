d52def48de3520e0877215448e800df089e88d11
JoseAl05 <jparancibialinker@gmail.com>
2023-05-23 22:37:53 -0400

    add dynamic import to map and minor style changes

--- app/components/circuits/CircuitCard.tsx
+++ app/components/circuits/CircuitCard.tsx
@@ -1,61 +1,84 @@
 'use client'
 
-import useImage from '@/app/hooks/useImage';
 import { CircuitData } from '@/app/types';
 import axios from 'axios';
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
+import { useMemo } from 'react';
 import { BiWorld } from 'react-icons/bi';
 import { HiLocationMarker } from 'react-icons/hi';
-import Map from '../map/Map';
+import Button from '../buttons/Button';
+import { CircuitResponse } from '@/app/types/CircuitTypes';
 
 interface CircuitCardProps {
-    circuit: CircuitData;
+    circuit: CircuitResponse;
     circuitPerPage: number;
+    year:string;
 }
 
-const CircuitCard: React.FC<CircuitCardProps> = ({ circuit, circuitPerPage }) => {
+const CircuitCard: React.FC<CircuitCardProps> = ({ circuit, circuitPerPage,year }) => {
 
 
-    const getImage = useImage({
-        title: circuit.circuitName
-    })
+    const Map = useMemo(() => dynamic(() => import('../map/Map'), {
+        ssr: false,
+    }), [])
 
-    if (getImage.error) {
-        return (
-            <div>Error</div>
-        )
-    }
-
-    if (getImage.isLoading) {
-        return (
-            <div>Loading...</div>
-        )
-    }
 
     const coordinates = [
-        parseFloat(circuit.Location.lat),
-        parseFloat(circuit.Location.long)
+        parseFloat(circuit.lat),
+        parseFloat(circuit.lng)
     ];
 
     return (
-        <div className='col-span-1'>
-            <div className='flex flex-col gap-8 w-full border border-1 p-5 rounded-lg border-neutral-300 overflow-hidden'>
-                <h1 className='text-xl font-semibold text-black'>{circuit.circuitName}</h1>
+        <>
+            <h1 className='text-xl font-semibold text-black'>{circuit.name}</h1>
+            <div className='flex flex-row justify-between items-center gap-8 w-full'>
                 <Map
                     center={coordinates}
                 />
-                <div className='flex flex-row gap-2'>
-                    <BiWorld size={24} />
-                    <span>{circuit.Location.country}</span>
+                <div className='flex flex-col items-center gap-5'>
+                    <div className='flex flex-row gap-2'>
+                        <BiWorld size={24} />
+                        <span>{circuit.country}</span>
+                        <HiLocationMarker size={24} />
+                        <span>{circuit.location}</span>
+                    </div>
+                    <div className='flex flex-row justify-center items-center gap-8'>
+                        <a
+                            href={circuit.url}
+                            className='
+                                p-2
+                                text-lg
+                                font-light
+                                text-black
+                                text-center
+                                bg-transparent
+                                border
+                                border-black
+                                rounded-lg
+                                transition
+                                hover:bg-red-500
+                                hover:scale-105
+                                '
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            Más Información
+                        </a>
+                        {
+                            year && (
+                                <Button
+                                    label='Ver datos'
+                                    onClick={() => {}}
+                                    backgroundColor
+                                    small
+                                />
+                            )
+                        }
+                    </div>
                 </div>
-                <hr />
-                <div className='flex flex-row gap-2'>
-                    <HiLocationMarker size={24} />
-                    <span>{circuit.Location.locality}</span>
-                </div>
-                <a href={circuit.url} className='text-lg font-light text-white text-center bg-neutral-400 p-1 rounded-lg transition hover:bg-red-500 hover:scale-105'>Go to Wiki</a>
             </div>
-        </div>
+        </>
     );
 }
 
