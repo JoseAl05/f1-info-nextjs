9083de233ad52ab049cf6452c3cdebe0f8357532
JoseAl05 <jparancibialinker@gmail.com>
2023-05-23 22:41:43 -0400

    add link to season page

--- app/components/navbar/Menu.tsx
+++ app/components/navbar/Menu.tsx
@@ -4,7 +4,7 @@ const Menu = () => {
     return (
         <>
             <Link href='/circuits?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Circuitos</Link>
-            <p className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Temporadas</p>
+            <Link href='/seasons?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Temporadas</Link>
             <p className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Pilotos</p>
             <p className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Constructores</p>
         </>
