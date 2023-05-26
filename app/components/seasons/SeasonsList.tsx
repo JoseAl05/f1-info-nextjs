90bf4b06d199952c6b6cc6aa80101adf5f88da9e
JoseAl05 <jparancibialinker@gmail.com>
2023-05-25 23:52:00 -0400

    delete modal hook and change props of pagination component call

--- app/components/seasons/SeasonsList.tsx
+++ app/components/seasons/SeasonsList.tsx
@@ -1,58 +1,40 @@
 'use client'
 
-import useSeason from '@/app/hooks/useSeason';
 import { useRouter, useSearchParams } from 'next/navigation';
 import SeasonCard from './SeasonCard';
 import Pagination from '../pagination/Pagination';
 import Button from '../buttons/Button';
-import useDriverModal from '@/app/hooks/useDriverModal';
+import { SeasonResponse } from '@/app/types/SeasonTypes';
 
-const SeasonsList = () => {
+interface SeasonListProps {
+    seasons?:SeasonResponse[];
+    qSeasons?:number;
+}
 
+const SeasonsList:React.FC<SeasonListProps> = ({seasons,qSeasons}) => {
+
+    const seasonsPerPage = 12;
     const params = useSearchParams();
     const router = useRouter();
-    const driverModal = useDriverModal();
-
-
-    const { seasons, qSeasons, error, isLoading } = useSeason({ page: params.get('page') });
-
-    if (error) {
-        return (
-            <div>Error</div>
-        )
-    }
-
-    if (isLoading) {
-        return (
-            <div>Loading...</div>
-        )
-    }
 
 
     return (
         <>
-            <Button
-                label='Filtrar'
-                onClick={driverModal.onOpen}
-                small
-            />
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 pt-10'>
-                {seasons?.SeasonTable.Seasons.map((season) => {
+                {seasons?.map((season) => {
                     return (
                         <SeasonCard
                             key={season.season}
                             season={season}
-                            seasonPerPage={seasons.limit}
+                            seasonPerPage={seasonsPerPage}
                         />
                     )
                 })}
             </div>
             <Pagination
                 count={qSeasons}
-                dataPerPage={parseInt(seasons.limit)}
-                queryParams={{
-                    page: '?page'
-                }}
+                dataPerPage={seasonsPerPage}
+                currentPathname='/seasons'
             />
         </>
     );
