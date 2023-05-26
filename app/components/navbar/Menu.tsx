5de070c3549013f4894c840d4f9f326242dffeb7
JoseAl05 <jparancibialinker@gmail.com>
2023-05-26 00:57:20 -0400

    add redirect to driver page

--- app/components/navbar/Menu.tsx
+++ app/components/navbar/Menu.tsx
@@ -5,7 +5,7 @@ const Menu = () => {
         <>
             <Link href='/circuits?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Circuitos</Link>
             <Link href='/seasons?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Temporadas</Link>
-            <p className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Pilotos</p>
+            <Link href='/drivers?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Pilotos</Link>
             <p className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Constructores</p>
         </>
     );
