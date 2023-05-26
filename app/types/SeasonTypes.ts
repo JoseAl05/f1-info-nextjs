64de1d757455c8b3cf32c2a201b267fec36a7dbb
JoseAl05 <jparancibialinker@gmail.com>
2023-05-25 23:45:05 -0400

    change season types

--- app/types/SeasonTypes.ts
+++ app/types/SeasonTypes.ts
@@ -1,24 +1,5 @@
-export type Season = {
-    MRData: SeasonResponse;
-};
-
 export type SeasonResponse = {
-    xmlns: string;
-    series: string;
-    limit: number;
-    offset: number;
-    total: number;
-    SeasonTable: SeasonTable;
-};
-
-export type SeasonTable = {
-    constructorId?: string;
-    driverId?: string;
-    Seasons: SeasonData[];
-};
-
-export type SeasonData = {
-    season: string;
-    url: string;
-};
+    year:number;
+    url:string;
+}
 
