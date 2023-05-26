c240e28b97f4d792171681dd951071d2cb03a913
JoseAl05 <jparancibialinker@gmail.com>
2023-05-25 23:51:23 -0400

    minor change on structure and styles

--- app/components/seasons/SeasonCard.tsx
+++ app/components/seasons/SeasonCard.tsx
@@ -1,17 +1,42 @@
 'use client'
 
-import { SeasonData } from '@/app/types/SeasonTypes';
+import { SeasonResponse } from '@/app/types/SeasonTypes';
+import { useRouter } from 'next/navigation';
 
 interface SeasonCardProps {
-    season: SeasonData;
+    season: SeasonResponse;
     seasonPerPage: number;
 }
 
 const SeasonCard: React.FC<SeasonCardProps> = ({ season, seasonPerPage }) => {
+
+    const router = useRouter();
+
     return (
         <>
-            <div className='flex flex-col justify-between items-center gap-8 w-full border border-black rounded-xl p-10'>
-                    <h1 className='text-xl font-semibold text-black'>Temporada de {season.season}</h1>
+            <div className='
+                flex
+                flex-col
+                justify-between
+                items-center
+                gap-8
+                w-full
+                border
+                border-black
+                rounded-xl
+                p-10
+                cursor-pointer
+                transition
+                hover:scale-110
+                hover:bg-[#D72323]
+                group
+                '
+                onClick={(e) => {
+                    router.push(`/seasons/${season.year}?page=0`)
+                    e.preventDefault();
+                }}
+            >
+                    <h1 className='text-xl font-semibold text-black transition group-hover:text-white'>Temporada de {season.year}</h1>
                     <a
                         href={season.url}
                         className='
@@ -27,6 +52,7 @@ const SeasonCard: React.FC<SeasonCardProps> = ({ season, seasonPerPage }) => {
                             transition
                             hover:bg-red-500
                             hover:scale-105
+                            group-hover:text-white
                         '
                         target='_blank'
                         rel='noreferrer'
