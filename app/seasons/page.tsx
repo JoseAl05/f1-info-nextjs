af5ef4a468f9858257fa2ae62c9317f419d1312b
JoseAl05 <jparancibialinker@gmail.com>
2023-05-25 23:46:43 -0400

    change name and ui of season page

--- app/seasons/page.tsx
+++ app/seasons/page.tsx
@@ -1,17 +1,26 @@
 import Container from '../components/Container';
 import SelectDriverModal from '../components/modals/SelectDriverModal';
 import SeasonsList from '../components/seasons/SeasonsList';
+import getSeasons from '../functions/getSeasons';
 
-const CircuitPage = () => {
+const SeasonPage = async({searchParams}:{searchParams: { [key: string]: string | undefined }}) => {
+
+    const seasonsPerPage = 12;
+    const currentPage = parseInt(searchParams.page);
+
+    const seasons = await getSeasons({seasonsPerPage:seasonsPerPage,currentPage:currentPage});
+
     return (
         <Container>
             <div className='flex flex-col items-center py-48'>
                 <h1 className='text-black text-3xl font-bold'>Temporadas</h1>
-                <SelectDriverModal/>
-                <SeasonsList />
+                <SeasonsList
+                    seasons={seasons.seasons}
+                    qSeasons={seasons.qSeasons}
+                />
             </div>
         </Container>
     );
 }
 
-export default CircuitPage;
+export default SeasonPage;
