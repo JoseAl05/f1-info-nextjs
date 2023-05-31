3ef0b2fdf499422cfa3c8c07762da1470bb78f49
JoseAl05 <jparancibialinker@gmail.com>
2023-05-30 22:16:54 -0400

    minor fix on wiki end point

--- app/hooks/useImage.ts
+++ app/hooks/useImage.ts
@@ -8,11 +8,13 @@ interface IUseImage {
 
 const useImage = ({ title }: IUseImage) => {
 
-  const formattedTitle = title?.split(' ').join('%20');
+  const formattedTitle = title?.split(' ').join('_');
+
 
   // const wikiEndPoint = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages&indexpageids=1&titles=${formattedTitle}&piprop=original`;
 
-  const wikiEndPoint = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages&indexpageids=1&titles=${title}&piprop=original%7Cthumbnail&pithumbsize=400`;
+  const wikiEndPoint = `https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=pageimages&indexpageids=1&titles=${formattedTitle}&piprop=original`;
+
 
   const fetcher = async (url) => await axios.get(url).then((res) => res.data);
   const { data, error, isLoading } = useSWR(wikiEndPoint, fetcher);
