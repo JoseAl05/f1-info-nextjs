cea071a9a6c2317bd89d14768d787be014e89696
JoseAl05 <jparancibialinker@gmail.com>
2023-05-30 22:18:54 -0400

    add constructors to navbar menu

--- app/components/navbar/Menu.tsx
+++ app/components/navbar/Menu.tsx
@@ -6,7 +6,7 @@ const Menu = () => {
             <Link href='/circuits?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Circuitos</Link>
             <Link href='/seasons?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Temporadas</Link>
             <Link href='/drivers?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Pilotos</Link>
-            <p className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Constructores</p>
+            <Link href='/constructors?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Constructores</Link>
         </>
     );
 }
