36c12c2fc096f7bde2cf78d330b5ac7f16d7f5c7
JoseAl05 <jparancibialinker@gmail.com>
2023-05-30 22:18:22 -0400

    create constructors page

---
+++ app/constructors/page.tsx
@@ -0,0 +1,25 @@
+import Container from '../components/Container';
+import ConstructorList from '../components/constructors/ConstructorList';
+import { getConstructors } from '../functions/getConstructors';
+
+const ConstructorsPage = async({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
+
+    const constructorsPerPage = 12;
+    const currentPage = parseInt(searchParams.page);
+
+    const constructors = await getConstructors({currentPage:currentPage,constructorsPerPage:constructorsPerPage});
+
+    return (
+        <Container>
+            <div className='flex flex-col items-center py-48'>
+                <h1 className='text-black text-3xl font-bold'>Constructores</h1>
+                <ConstructorList
+                    constructors={constructors.constructors}
+                    qConstructors={constructors.qConstructors}
+                />
+            </div>
+        </Container>
+    );
+}
+
+export default ConstructorsPage;
