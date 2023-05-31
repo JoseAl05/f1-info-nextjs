5db6008ff6afbdfea8b43f3f0ea9855055f25391
JoseAl05 <jparancibialinker@gmail.com>
2023-05-30 22:19:52 -0400

    create an alphabet filter component

---
+++ app/components/inputs/AlphabetFilter.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+interface AlphaberFilterProps {
+    onClick: (value: string) => void;
+    selected?: boolean;
+    label: string;
+    disabled?:boolean;
+}
+
+const AlphabetFilter: React.FC<AlphaberFilterProps> = ({ onClick, selected, label,disabled }) => {
+    return (
+        <>
+            {
+                !disabled ? (
+                    <div
+                        onClick={() => onClick(label)}
+                        className={`
+                                w-full
+                                rounded-xl
+                                border-2
+                                p-4
+                                flex
+                                flex-col
+                                gap-3
+                                cursor-pointer
+                                transition
+                                hover:border-[#D72323]
+                                ${selected ? 'border-[#D72323]' : 'border-neutral-200'}
+                            `}
+                    >
+                        <p className='font-semibold'>
+                            {label}
+                        </p>
+                    </div>
+
+                ):(
+                    <div
+                        className='
+                                w-full
+                                rounded-xl
+                                border-2
+                                p-4
+                                flex
+                                flex-col
+                                gap-3
+                                cursor-not-allowed
+                            '
+                    >
+                        <p className='font-semibold'>
+                            {label}
+                        </p>
+                    </div>
+                )
+            }
+        </>
+    );
+}
+
+export default AlphabetFilter;
