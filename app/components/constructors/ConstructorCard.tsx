cbe225dd78dac64097de328ab8bd4ae0a9032ee5
JoseAl05 <jparancibialinker@gmail.com>
2023-05-30 22:21:52 -0400

    create constructor card component

---
+++ app/components/constructors/ConstructorCard.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { DriverResponse } from '@/app/types/DriverTypes';
+import { BiWorld } from 'react-icons/bi';
+import { AiOutlineFieldNumber } from 'react-icons/ai';
+import { GiRaceCar } from 'react-icons/gi';
+import { CiCalendarDate } from 'react-icons/ci';
+import { FaWikipediaW } from 'react-icons/fa';
+import { ConstructorResponse } from '@/app/types/ConstructorTypes';
+import ConstructorImage from './ConstructorImage';
+
+interface ConstructorCardProps {
+    constructor: ConstructorResponse;
+    constructorsPerPage: number;
+}
+
+const ConstructorCard: React.FC<ConstructorCardProps> = ({ constructor, constructorsPerPage }) => {
+
+    return (
+        <>
+            <div className='flex flex-col items-start gap-5 border border-black rounded-xl p-5 cursor-pointer transition duration-500 hover:-translate-y-3 hover:bg-[#D72323] group w-full'>
+                <h1 className='text-xl font-semibold text-black transition group-hover:text-white'>{constructor.name}</h1>
+                <hr className='w-full border-black' />
+                <div className='flex flex-row gap-2'>
+                    <BiWorld size={24} className='group-hover:text-white' />
+                    <span className='group-hover:text-white'>{constructor.nationality}</span>
+                </div>
+                <ConstructorImage
+                    constructor={constructor}
+                />
+                <span className='invisible group-hover:visible group-hover:mx-auto group-hover:font-bold group-hover:text-xl group-hover:text-white'>Ver resultados</span>
+            </div>
+        </>
+    );
+}
+
+export default ConstructorCard;
