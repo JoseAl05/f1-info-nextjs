51bb9c8ea8202980d539e3fe453108826f7f9ff6
JoseAl05 <jparancibialinker@gmail.com>
2023-05-30 22:18:13 -0400

    add a driver filter component to drivers UI and more params

--- app/drivers/page.tsx
+++ app/drivers/page.tsx
@@ -1,18 +1,23 @@
 import Container from '../components/Container';
 import DriverList from '../components/drivers/DriverList';
+import DriversFilter from '../components/inputs/DriversFilter';
 import { getDrivers } from '../functions/getDrivers';
 
 const DriversPage = async({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
 
-    const driversPerPage = 12;
+    const driversPerPage = searchParams.dataPerPage ? parseInt(searchParams.dataPerPage) : 12;
     const currentPage = parseInt(searchParams.page);
+    const sortDriversByForename = searchParams.sortByLetter ? searchParams.sortByLetter : '';
+    const driverForename = searchParams.driverForename ? searchParams.driverForename : null;
+    const driverSurname = searchParams.driverSurname ? searchParams.driverSurname : null;
 
-    const drivers = await getDrivers({currentPage:currentPage,driversPerPage:driversPerPage});
+    const drivers = await getDrivers({currentPage:currentPage,driversPerPage:driversPerPage,sortByLetter:sortDriversByForename,driverForename:driverForename,driverSurname:driverSurname});
 
     return (
         <Container>
-            <div className='flex flex-col items-center py-48'>
-                <h1 className='text-black text-3xl font-bold'>Pilotos</h1>
+            <div className='flex flex-col items-stretch py-48 gap-5'>
+                <h1 className='text-black text-3xl text-center font-bold'>Pilotos</h1>
+                <DriversFilter />
                 <DriverList
                     drivers={drivers.drivers}
                     qDrivers={drivers.qDrivers}
