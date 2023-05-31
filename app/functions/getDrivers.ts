2e11a7a496995aae073ca5e280870d0dff0a7015
JoseAl05 <jparancibialinker@gmail.com>
2023-05-30 22:17:08 -0400

    add more querys to get drivers function

--- app/functions/getDrivers.ts
+++ app/functions/getDrivers.ts
@@ -2,30 +2,48 @@ import prisma from '@/app/libs/prismadb';
 import { DriverResponse } from '../types/DriverTypes';
 
 export interface IDriverParams {
-    driversPerPage: number;
-    currentPage: number;
-  }
+  driversPerPage: number;
+  currentPage: number;
+  sortByLetter?: string;
+  driverForename?: string | null;
+  driverSurname?: string | null;
+}
 
-export async function getDrivers(params:IDriverParams) {
+export async function getDrivers(params: IDriverParams) {
   try {
+    const { driversPerPage, currentPage, sortByLetter, driverForename , driverSurname} = params;
 
-    const {driversPerPage,currentPage} = params;
+    let query = {};
 
-    const qDrivers = await prisma.drivers.count();
-
-    const drivers = await prisma.drivers.findMany({
-        skip:currentPage,
-        take:driversPerPage,
-        orderBy:{
-            forename:'asc'
-        }
-    });
-
-    return{
-        drivers:drivers as DriverResponse[] | null,
-        qDrivers:qDrivers as number | null,
+    if (sortByLetter?.length !== 0) {
+      query.forename = {
+        startsWith: sortByLetter,
+      };
+    }
+    if (driverForename) {
+      query.forename = driverForename;
     }
 
+    if(driverSurname){
+      query.surname = driverSurname;
+    }
+
+    const qDrivers = await prisma.drivers.count({
+      where: query,
+    });
+    const drivers = await prisma.drivers.findMany({
+      skip: currentPage,
+      take: driversPerPage,
+      where: query,
+      orderBy: {
+        forename: 'asc',
+      },
+    });
+
+    return {
+      drivers: drivers as DriverResponse[] | null,
+      qDrivers: qDrivers as number | null,
+    };
   } catch (error: any) {
     throw new Error(error.message);
   }
