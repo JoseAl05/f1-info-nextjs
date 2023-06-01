d19bc66a15d01326c31e2936782cd34ccc15d0b6
JoseAl05 <jparancibialinker@gmail.com>
2023-05-31 23:46:03 -0400

    create season image component

---
+++ app/components/seasons/SeasonImage.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import useImage from '@/app/hooks/useImage';
+import Image from 'next/image';
+import placeholder from '/public/images/placeholder.png';
+
+interface SeasonImageProps {
+    seasonYear?:number;
+}
+
+const SeasonImage:React.FC<SeasonImageProps> = ({seasonYear}) => {
+
+    let title = ''
+    if(seasonYear > 1980){
+        title = `${seasonYear}_Formula_One_World_Championship`;
+    } else {
+        title = `${seasonYear}_Formula_One_season`;
+    }
+
+    const { imageData, error, isLoading } = useImage({ title: title });
+    const imageId = imageData?.pageids[0];
+
+    if (isLoading) {
+        return(
+            <p>
+                Loading...
+            </p>
+        )
+    }
+
+    if (error) {
+        return(
+            <p>
+                Error.
+            </p>
+        )
+    }
+
+    return (
+        <div className='w-full relative overflow-hidden rounded-xl grayscale group-hover:grayscale-0'>
+            <Image
+                alt={`Season ${seasonYear}`}
+                src={imageData?.pages[imageId].original ? imageData?.pages[imageId].original?.source : placeholder}
+                width={imageData?.pages[imageId].original ? imageData?.pages[imageId].original.width : 0}
+                height={imageData?.pages[imageId].original ? imageData?.pages[imageId].original.height : 0}
+                className='object-cover h-full w-full'
+            />
+        </div>
+    );
+}
+
+export default SeasonImage;
