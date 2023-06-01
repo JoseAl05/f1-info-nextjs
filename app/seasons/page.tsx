b93d241877b0c4cdc3d20de1709c045ad6ebcaac
JoseAl05 <jparancibialinker@gmail.com>
2023-05-31 23:45:15 -0400

    add season filters

--- app/seasons/page.tsx
+++ app/seasons/page.tsx
@@ -1,19 +1,24 @@
 import Container from '../components/Container';
+import SeasonsFilter from '../components/inputs/SeasonsFilter';
 import SelectDriverModal from '../components/modals/SelectDriverModal';
 import SeasonsList from '../components/seasons/SeasonsList';
 import getSeasons from '../functions/getSeasons';
 
-const SeasonPage = async({searchParams}:{searchParams: { [key: string]: string | undefined }}) => {
+const SeasonPage = async ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
 
-    const seasonsPerPage = 12;
+    const seasonsPerPage = searchParams.dataPerPage ? parseInt(searchParams.dataPerPage) : 12;
+    const decade = searchParams.decade ? parseInt(searchParams.decade) : null;
     const currentPage = parseInt(searchParams.page);
 
-    const seasons = await getSeasons({seasonsPerPage:seasonsPerPage,currentPage:currentPage});
+    const seasons = await getSeasons({ seasonsPerPage: seasonsPerPage, currentPage: currentPage, decade: decade });
 
     return (
         <Container>
-            <div className='flex flex-col items-center py-48'>
-                <h1 className='text-black text-3xl font-bold'>Temporadas</h1>
+            <div className='flex flex-col items-stretch py-48 gap-5'>
+                <h1 className='text-black text-3xl text-center font-bold'>Temporadas</h1>
+                <SeasonsFilter
+                    seasons={seasons.seasons}
+                />
                 <SeasonsList
                     seasons={seasons.seasons}
                     qSeasons={seasons.qSeasons}
