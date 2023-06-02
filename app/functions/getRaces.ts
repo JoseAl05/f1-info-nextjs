5aa7fd57a2d41fdafeefe202ca7ef92482580134
JoseAl05 <jparancibialinker@gmail.com>
2023-06-01 22:50:01 -0400

    change the export and add a new function

--- app/functions/getRaces.ts
+++ app/functions/getRaces.ts
@@ -2,46 +2,80 @@ import prisma from '@/app/libs/prismadb';
 import { RaceResponse } from '../types/RaceTypes';
 
 export interface IRaceParams {
-    racesPerPage: number;
-    currentPage: number;
-    circuitId:string;
-    year?:number;
+  racesPerPage: number;
+  currentPage: number;
+  circuitId: string;
+  year?: number;
 }
 
-export default async function getRaces(params:IRaceParams){
-    try {
-        const { racesPerPage, currentPage,circuitId,year} = params;
-        let query = {};
+export interface IRaceByIdParams {
+  raceId: number;
+}
 
-        //Si se seleccionó un año.
-        //Se añade a la query.
-        if(year){
-            query.year = parseInt(year)
-        }
+export async function getRaces(params: IRaceParams) {
+  try {
+    const { racesPerPage, currentPage, circuitId, year } = params;
+    let query = {};
 
-        //Se obtiene la cantidad de carreras con el filtro aplicado.
-        const qRaces = await prisma.races.count({
-            where:query
-        });
-
-        const races = await prisma.races.findMany({
-            skip: currentPage,
-            take: racesPerPage,
-            where:query,
-            include:{
-                circuits:true,
-            },
-            orderBy: {
-              round: 'asc',
-            },
-        })
-
-        return{
-            races:races as RaceResponse[] | null,
-            qRaces:qRaces as number | null,
-        }
-
-    } catch (error:any) {
-        throw new Error(error.message);
+    //Si se seleccionó un año.
+    //Se añade a la query.
+    if (year) {
+      query.year = parseInt(year);
     }
+
+    //Se obtiene la cantidad de carreras con el filtro aplicado.
+    const qRaces = await prisma.races.count({
+      where: query,
+    });
+
+    const races = await prisma.races.findMany({
+      skip: currentPage,
+      take: racesPerPage,
+      where: query,
+      include: {
+        circuits: true,
+      },
+      orderBy: {
+        round: 'asc',
+      },
+    });
+
+    return {
+      races: races as RaceResponse[] | null,
+      qRaces: qRaces as number | null,
+    };
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
+
+export async function getRaceById(params: IRaceByIdParams) {
+  try {
+    const { raceId } = params;
+
+    let query = {};
+
+    if (raceId) {
+      query.raceId = raceId;
+    }
+
+    const race = await prisma.races.findUnique({
+        where:query,
+        include:{
+            qualifying:true,
+            results:true
+        }
+    })
+
+    if(!race?.qualifying){
+      return null;
+    }
+
+    return {
+        race: race as RaceResponse | null
+    }
+
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 }
