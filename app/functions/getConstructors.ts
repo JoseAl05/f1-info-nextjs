6535ca4e4b2439f3a60117b412c64048f272edd8
JoseAl05 <jparancibialinker@gmail.com>
2023-06-01 22:50:35 -0400

    add a new function

--- app/functions/getConstructors.ts
+++ app/functions/getConstructors.ts
@@ -2,28 +2,53 @@ import prisma from '@/app/libs/prismadb';
 import { ConstructorResponse } from '../types/ConstructorTypes';
 
 export interface IConstructorParams {
-    constructorsPerPage: number;
-    currentPage: number;
-  }
+  constructorsPerPage: number;
+  currentPage: number;
+}
 
-export async function getConstructors(params:IConstructorParams) {
+export interface IContructorByIdParams {
+  constructorId: number;
+}
+
+export async function getConstructors(params: IConstructorParams) {
   try {
-
-    const {constructorsPerPage,currentPage} = params;
+    const { constructorsPerPage, currentPage } = params;
 
     const qConstructors = await prisma.constructors.count();
 
     const constructors = await prisma.constructors.findMany({
-        skip:currentPage,
-        take:constructorsPerPage,
-        orderBy:{
-            name:'asc'
-        }
+      skip: currentPage,
+      take: constructorsPerPage,
+      orderBy: {
+        name: 'asc',
+      },
     });
 
+    return {
+      constructors: constructors as ConstructorResponse[] | null,
+      qConstructors: qConstructors as number | null,
+    };
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
+
+export async function getConstructorById(params: IContructorByIdParams) {
+  try {
+    const { constructorId } = params;
+
+    let query = {};
+
+    if (constructorId) {
+      query.constructorId = constructorId;
+    }
+
+    const constructor = await prisma.constructors.findUnique({
+      where:query
+    })
+
     return{
-        constructors:constructors as ConstructorResponse[] | null,
-        qConstructors:qConstructors as number | null,
+      constructor:constructor as ConstructorResponse | null
     }
 
   } catch (error: any) {
