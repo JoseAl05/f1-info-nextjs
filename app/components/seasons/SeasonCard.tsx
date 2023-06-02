696013194388dd04796d6796bd2ea200de2523ae
JoseAl05 <jparancibialinker@gmail.com>
2023-06-01 22:50:43 -0400

    minor structure change

--- app/components/seasons/SeasonCard.tsx
+++ app/components/seasons/SeasonCard.tsx
@@ -14,8 +14,7 @@ const SeasonCard: React.FC<SeasonCardProps> = ({ season, seasonPerPage }) => {
     const router = useRouter();
 
     return (
-        <>
-            <div className='
+        <div className='
                 flex
                 flex-col
                 justify-between
@@ -32,39 +31,16 @@ const SeasonCard: React.FC<SeasonCardProps> = ({ season, seasonPerPage }) => {
                 hover:bg-[#D72323]
                 group
                 '
-                onClick={(e) => {
-                    router.push(`/seasons/${season.year}?page=0`)
-                    e.preventDefault();
-                }}
-            >
-                    <h1 className='text-xl font-semibold text-black transition group-hover:text-white'>Temporada de {season.year}</h1>
-                    <SeasonImage
-                        seasonYear={season.year}
-                    />
-                    <a
-                        href={season.url}
-                        className='
-                            p-2
-                            text-lg
-                            font-light
-                            text-black
-                            text-center
-                            bg-transparent
-                            border
-                            border-black
-                            rounded-lg
-                            transition
-                            hover:bg-red-500
-                            hover:scale-105
-                            group-hover:text-white
-                        '
-                        target='_blank'
-                        rel='noreferrer'
-                    >
-                        Más Información
-                    </a>
-            </div>
-        </>
+            onClick={(e) => {
+                router.push(`/seasons/${season.year}?page=0`)
+                e.preventDefault();
+            }}
+        >
+            <h1 className='text-xl font-semibold text-black transition group-hover:text-white'>Temporada de {season.year}</h1>
+            <SeasonImage
+                seasonYear={season.year}
+            />
+        </div>
     );
 }
 
