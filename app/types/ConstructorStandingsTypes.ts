ae9072cd84e6bb2177469300ba57621e59b417af
JoseAl05 <jparancibialinker@gmail.com>
2023-06-10 22:51:37 -0400

    create constructor standings types

---
+++ app/types/ConstructorStandingsTypes.ts
@@ -0,0 +1,9 @@
+export type ConstructorStandingsResponse = {
+    constructorStandingsId:number;
+    raceId:number;
+    constructorId:number;
+    points:number;
+    position?:number;
+    positionText?:string;
+    wins:number;
+}
