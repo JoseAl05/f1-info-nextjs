3bf426229e07a212b14d1713ab7d1051b4eeec31
JoseAl05 <jparancibialinker@gmail.com>
2023-06-10 22:54:04 -0400

    minor styles changes

--- app/components/seasons/Seasons.tsx
+++ app/components/seasons/Seasons.tsx
@@ -5,22 +5,24 @@ import Button from '../buttons/Button';
 
 const Seasons = () => {
     return (
-        <div className='bg-[#D72323] p-20 w-full bg-opacity-90'>
-            <div className='flex flex-col items-center gap-5'>
-                <h1 className='text-3xl font-bold text-white'>Temporadas</h1>
-                <p className='text-lg font-light text-white break-words max-w-3xl'>
+        <div className='bg-black bg-opacity-90'>
+
+            <div className='flex flex-col items-center gap-8 p-5'>
+                <h1 className='text-xl md:text-2xl lg:text-3xl font-bold text-neutral-300 uppercase'>Temporadas</h1>
+                <p className='text-sm md:text-base lg:text-lg font-light text-neutral-300 max-w-4xl'>
                     Sumérgete en el pasado y revive las temporadas más emocionantes de la Fórmula 1.
                     Acompaña a los pilotos en su lucha por el campeonato, conoce los momentos históricos que definieron las carreras y
                     explora las estadísticas que dejaron huella en el deporte.
                 </p>
                 <Button
-                    label='Explorar temporadas!'
+                    label='Ver más'
                     onClick={() => { }}
                     disabled={false}
                     small
                     backgroundColor={false}
                 />
             </div>
+
         </div>
     );
 }
