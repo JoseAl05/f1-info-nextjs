83a5d5ed0fb20b84ffab4212b64aa637d86b3f5d
JoseAl05 <jparancibialinker@gmail.com>
2023-06-10 22:56:24 -0400

    change style for more responsive UI

--- app/components/drivers/Drivers.tsx
+++ app/components/drivers/Drivers.tsx
@@ -7,19 +7,19 @@ const Drivers = () => {
     return (
         <Container>
             <div className='flex flex-col items-center gap-8'>
-                <h1 className='text-3xl font-bold text-black'>Pilotos</h1>
-                <p className='text-lg font-light text-black break-words w-full'>
+                <h1 className='text-xl md:text-2xl lg:text-3xl font-bold text-black'>Pilotos</h1>
+                <p className='text-sm md:text-base lg:text-lg font-light text-black break-words max-w-4xl'>
                     Conoce a las leyendas de la Fórmula 1 que han dejado una huella imborrable en la historia del deporte.
                     Desde los intrépidos pioneros hasta los campeones indiscutibles, te presentamos perfiles detallados de los pilotos más exitosos y carismáticos.
                     Descubre sus logros, carreras destacadas y estadísticas impresionantes.
                 </p>
-                <Button
+                {/* <Button
                     label='Explorar pilotos!'
                     onClick={() => { }}
                     disabled={false}
                     small
                     backgroundColor
-                />
+                /> */}
             </div>
         </Container>
     );
