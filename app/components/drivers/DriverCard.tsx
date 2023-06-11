06b5b192f9cc1c93ad04c2ef1bb5e53d8ec6526b
JoseAl05 <jparancibialinker@gmail.com>
2023-06-10 22:57:14 -0400

    add empty state component and redirect to drivers page

--- app/components/drivers/DriverCard.tsx
+++ app/components/drivers/DriverCard.tsx
@@ -6,10 +6,12 @@ import { AiOutlineFieldNumber } from 'react-icons/ai';
 import { GiRaceCar } from 'react-icons/gi';
 import { CiCalendarDate } from 'react-icons/ci';
 import { FaWikipediaW } from 'react-icons/fa';
-import { RxCross1 } from 'react-icons/rx';
+
 import useImage from '@/app/hooks/useImage';
 import Image from 'next/image';
 import DriverImage from './DriverImage';
+import { useRouter } from 'next/navigation';
+import EmptyState from '../emptyState/EmptyState';
 
 interface DriverCardProps {
     driver: DriverResponse;
@@ -18,9 +20,30 @@ interface DriverCardProps {
 
 const DriverCard: React.FC<DriverCardProps> = ({ driver, driversPerPage }) => {
 
+    const router = useRouter();
+
     return (
         <>
-            <div className='flex flex-col items-start gap-5 border border-black rounded-xl p-5 cursor-pointer transition duration-500 hover:-translate-y-3 hover:bg-[#D72323] group w-full'>
+            <div
+                onClick={() => router.push(`/drivers/${driver.driverId}/${driver.forename}-${driver.surname}`)}
+                className='
+                    flex
+                    flex-col
+                    items-start
+                    gap-5
+                    border
+                    border-black
+                    rounded-xl
+                    p-5
+                    cursor-pointer
+                    transition
+                    duration-500
+                    hover:-translate-y-3
+                    hover:bg-[#D72323]
+                    group
+                    w-full
+                '
+            >
                 <h1 className='text-xl font-semibold text-black transition group-hover:text-white'>{driver.forename} {driver.surname}</h1>
                 <div className='flex flex-row gap-2'>
                     <BiWorld size={24} className='group-hover:text-white' />
@@ -28,6 +51,7 @@ const DriverCard: React.FC<DriverCardProps> = ({ driver, driversPerPage }) => {
                 </div>
                 <DriverImage
                     driver={driver}
+                    grayscale
                 />
                 <hr className='w-full border-black' />
                 <div className='flex flex-row gap-2'>
@@ -36,10 +60,7 @@ const DriverCard: React.FC<DriverCardProps> = ({ driver, driversPerPage }) => {
                         driver.number ? (
                             <span className='group-hover:text-white'>{driver.number}</span>
                         ) : (
-                            <div className='flex flex-row items-center gap-8'>
-                                <RxCross1 size={30} className='text-red-500 group-hover:text-white' />
-                                <span className='group-hover:text-white'>No Info</span>
-                            </div>
+                            <EmptyState />
                         )
                     }
                 </div>
@@ -50,10 +71,7 @@ const DriverCard: React.FC<DriverCardProps> = ({ driver, driversPerPage }) => {
                         driver.code ? (
                             <span className='group-hover:text-white'>{driver.code}</span>
                         ) : (
-                            <div className='flex flex-row items-center gap-8'>
-                                <RxCross1 size={30} className='text-red-500  group-hover:text-white' />
-                                <span className='group-hover:text-white'>No Info</span>
-                            </div>
+                            <EmptyState />
                         )
                     }
                 </div>
@@ -64,10 +82,7 @@ const DriverCard: React.FC<DriverCardProps> = ({ driver, driversPerPage }) => {
                         driver.dob ? (
                             <span className='group-hover:text-white'>{driver.dob?.toLocaleDateString()}</span>
                         ) : (
-                            <div className='flex flex-row items-center gap-8'>
-                                <RxCross1 size={30} className='text-red-500  group-hover:text-white' />
-                                <span className='group-hover:text-white'>No Info</span>
-                            </div>
+                            <EmptyState />
                         )
                     }
                 </div>
