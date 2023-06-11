5a2cc3d6eea8c6e4adec9be73e55a60d4a9c82b0
JoseAl05 <jparancibialinker@gmail.com>
2023-06-10 22:53:44 -0400

    create drivers page

---
+++ app/drivers/[driverId]/[driverName]/page.tsx
@@ -0,0 +1,93 @@
+import Container from '@/app/components/Container';
+import { getDriverById } from '@/app/functions/getDrivers';
+import DriverImage from '@/app/components/drivers/DriverImage';
+import DriversInfo from '@/app/components/drivers/DriversInfo';
+import EmptyState from '@/app/components/emptyState/EmptyState';
+
+interface DriversPageParams {
+    driverId?: number;
+    driverName?: string;
+}
+
+const DriversPage = async ({ params }: { params: DriversPageParams }) => {
+
+    const { driverId, driverName } = params;
+
+    const formattedDriverId = parseInt(driverId);
+
+    const driver = await getDriverById({ driverId: formattedDriverId });
+
+    return (
+        <Container>
+            <div className='flex flex-col items-center pt-48 pb-10'>
+                <h1 className='text-black text-3xl font-bold'>{driver.driver?.forename} {driver.driver?.surname}</h1>
+                <div className='flex flex-row justify-between items-center relative py-5 gap-8 max-w-2xl'>
+                    <DriverImage
+                        driver={driver.driver}
+                        grayscale={false}
+                    />
+                    <div className='flex flex-col items-center border border-black rounded-lg p-2 w-full'>
+                        <div className='flex flex-row justify-center items-center gap-5'>
+                            <h1 className='text-sm lg:text-xl font-semibold'>
+                                Nacionalidad:
+                            </h1>
+                            {
+                                driver.driver?.nationality ? (
+                                    <p className='text-sm lg:text-xl'>
+                                        {driver.driver?.nationality}
+                                    </p>
+                                ) : (
+                                    <EmptyState />
+                                )
+                            }
+                        </div>
+                        <div className='flex flex-row justify-center items-center gap-5'>
+                            <h1 className='text-sm lg:text-xl font-semibold'>
+                                Numero:
+                            </h1>
+                            {
+                                driver.driver?.number ? (
+                                    <p className='text-sm lg:text-xl'>
+                                        {driver.driver?.number}
+                                    </p>
+                                ) : (
+                                    <EmptyState />
+                                )
+                            }
+                        </div>
+                        <div className='flex flex-row justify-center items-center gap-5'>
+                            <h1 className='text-sm lg:text-xl font-semibold'>
+                                Codigo Piloto:
+                            </h1>
+                            {
+                                driver.driver?.code ? (
+                                    <p className='text-sm lg:text-xl'>
+                                        {driver.driver?.code}
+                                    </p>
+                                ) : (
+                                    <EmptyState />
+                                )
+                            }
+
+                        </div>
+                        <a
+                            href={driver.driver?.url}
+                            rel='noreferrer'
+                            target='_blank'
+                            className='text-sm mx-auto font-semibold text-blue-400 transition hover:text-blue-700 lg:text-xl'
+                        >
+                            Wikipedia
+                        </a>
+                    </div>
+                </div>
+                <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4 pt-10'>
+                    <DriversInfo
+                        driver={driver}
+                    />
+                </div>
+            </div>
+        </Container>
+    );
+}
+
+export default DriversPage;
