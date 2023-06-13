3ffd2ddf03f0e2a0cbd8f1598893376867c22bc5
JoseAl05 <jparancibialinker@gmail.com>
2023-06-13 17:37:04 -0400

    change UI for season section in home page

--- app/components/seasons/Seasons.tsx
+++ app/components/seasons/Seasons.tsx
@@ -1,28 +1,54 @@
-'use client'
-
+import Image from 'next/image';
 import Container from '../Container';
 import Button from '../buttons/Button';
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import seasonImage from '../../../public/images/seasonImage.png';
+import Link from 'next/link';
+
 
 const Seasons = () => {
     return (
-        <div className='bg-black bg-opacity-90'>
-
-            <div className='flex flex-col items-center gap-8 p-5'>
-                <h1 className='text-xl md:text-2xl lg:text-3xl font-bold text-neutral-300 uppercase'>Temporadas</h1>
-                <p className='text-sm md:text-base lg:text-lg font-light text-neutral-300 max-w-4xl'>
-                    Sumérgete en el pasado y revive las temporadas más emocionantes de la Fórmula 1.
-                    Acompaña a los pilotos en su lucha por el campeonato, conoce los momentos históricos que definieron las carreras y
-                    explora las estadísticas que dejaron huella en el deporte.
-                </p>
-                <Button
-                    label='Ver más'
-                    onClick={() => { }}
-                    disabled={false}
-                    small
-                    backgroundColor={false}
-                />
+        <div className='bg-gray-900'>
+            <div className='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
+                <div className='place-self-center mr-auto lg:col-span-7'>
+                    <h1 className='max-w-2xl mb-4 text-4xl text-white font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl'>Temporadas</h1>
+                    <p className='max-w-2xl mb-6 font-light text-gray-400 lg:mb-8 md:text-lg lg:text-xl'>
+                        Sumérgete en el pasado y revive las temporadas más emocionantes de la Fórmula 1.
+                        Acompaña a los pilotos en su lucha por el campeonato, conoce los momentos históricos que definieron las carreras y
+                        explora las estadísticas que dejaron huella en el deporte.
+                    </p>
+                    <Link
+                        href='/seasons?page=0'
+                        className='
+                            inline-flex
+                            items-center
+                            justify-center
+                            py-3
+                            mr-3
+                            text-base
+                            font-medium
+                            text-center
+                            text-white
+                            rounded-lg
+                            bg-primary-700
+                            transition
+                            hover:scale-105
+                            hover:bg-primary-800
+                            focus:ring-4
+                            focus:ring-primary-900
+                        '
+                    >
+                        Ir a temporadas
+                        <AiOutlineArrowRight size={20} color='white' />
+                    </Link>
+                </div>
+                <div className='hidden lg:mt-0 lg:col-span-5 lg:flex'>
+                    <Image
+                        alt='Season Image'
+                        src={seasonImage}
+                    />
+                </div>
             </div>
-
         </div>
     );
 }
