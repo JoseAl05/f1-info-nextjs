1ff6a42fdfb6609a5ed4ae4c9089d8fe8e6a2996
JoseAl05 <jparancibialinker@gmail.com>
2023-06-13 17:35:16 -0400

    add results to constructor type

--- app/types/ConstructorTypes.ts
+++ app/types/ConstructorTypes.ts
@@ -1,7 +1,10 @@
+import { ResultsResponse } from './ResultTypes';
+
 export type ConstructorResponse = {
     constructorId:number;
     constructorRef:string;
     name:string;
     nationality:string;
     url:string;
+    results?:ResultsResponse[];
 }
