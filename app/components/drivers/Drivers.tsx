2654405785755df3f232f5935cf74dd5352bf48b
JoseAl05 <jparancibialinker@gmail.com>
2023-06-13 17:45:47 -0400

    styles changes

--- app/components/drivers/Drivers.tsx
+++ app/components/drivers/Drivers.tsx
@@ -7,19 +7,12 @@ const Drivers = () => {
     return (
         <Container>
             <div className='flex flex-col items-center gap-8'>
-                <h1 className='text-xl md:text-2xl lg:text-3xl font-bold text-black'>Pilotos</h1>
-                <p className='text-sm md:text-base lg:text-lg font-light text-black break-words max-w-4xl'>
+                <h1 className='max-w-2xl mb-4 text-4xl text-black font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl'>Pilotos</h1>
+                <p className='max-w-4xl mb-6 font-light text-gray-700 lg:mb-8 md:text-lg lg:text-xl'>
                     Conoce a las leyendas de la Fórmula 1 que han dejado una huella imborrable en la historia del deporte.
                     Desde los intrépidos pioneros hasta los campeones indiscutibles, te presentamos perfiles detallados de los pilotos más exitosos y carismáticos.
                     Descubre sus logros, carreras destacadas y estadísticas impresionantes.
                 </p>
-                {/* <Button
-                    label='Explorar pilotos!'
-                    onClick={() => { }}
-                    disabled={false}
-                    small
-                    backgroundColor
-                /> */}
             </div>
         </Container>
     );
