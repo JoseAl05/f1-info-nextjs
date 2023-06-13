e2f0dd1acb906d1e4ff8ac172c454512c5c36b78
JoseAl05 <jparancibialinker@gmail.com>
2023-06-13 17:47:58 -0400

    add redirect to constructor page

--- app/components/constructors/ConstructorCard.tsx
+++ app/components/constructors/ConstructorCard.tsx
@@ -8,6 +8,7 @@ import { CiCalendarDate } from 'react-icons/ci';
 import { FaWikipediaW } from 'react-icons/fa';
 import { ConstructorResponse } from '@/app/types/ConstructorTypes';
 import ConstructorImage from './ConstructorImage';
+import { useRouter } from 'next/navigation';
 
 interface ConstructorCardProps {
     constructor: ConstructorResponse;
@@ -16,9 +17,30 @@ interface ConstructorCardProps {
 
 const ConstructorCard: React.FC<ConstructorCardProps> = ({ constructor, constructorsPerPage }) => {
 
+    const router = useRouter();
+
     return (
         <>
-            <div className='flex flex-col items-start gap-5 border border-black rounded-xl p-5 cursor-pointer transition duration-500 hover:-translate-y-3 hover:bg-[#D72323] group w-full'>
+            <div
+                onClick={() => router.push(`/constructors/${constructor.constructorId}/${constructor.name}`)}
+                className='
+                    flex
+                    flex-col
+                    items-start
+                    gap-5
+                    border
+                    border-black
+                    rounded-xl
+                    p-5
+                    cursor-pointer
+                    transition
+                    duration-500
+                    hover:-translate-y-3
+                    hover:bg-[#D72323]
+                    group
+                    w-full
+                '
+            >
                 <h1 className='text-xl font-semibold text-black transition group-hover:text-white'>{constructor.name}</h1>
                 <hr className='w-full border-black' />
                 <div className='flex flex-row gap-2'>
@@ -26,7 +48,7 @@ const ConstructorCard: React.FC<ConstructorCardProps> = ({ constructor, construc
                     <span className='group-hover:text-white'>{constructor.nationality}</span>
                 </div>
                 <ConstructorImage
-                    constructor={constructor}
+                    team={constructor}
                 />
                 <span className='invisible group-hover:visible group-hover:mx-auto group-hover:font-bold group-hover:text-xl group-hover:text-white'>Ver resultados</span>
             </div>
