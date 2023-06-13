91c0d292d74ec7eba937a7c344faf15bdfdb834f
JoseAl05 <jparancibialinker@gmail.com>
2023-06-13 17:47:34 -0400

    minor variable change

--- app/components/constructors/ConstructorImage.tsx
+++ app/components/constructors/ConstructorImage.tsx
@@ -7,12 +7,12 @@ import Image from 'next/image';
 import logo from '/public/images/logo.png';
 
 interface ConstructorImageProps {
-    constructor?:ConstructorResponse;
+    team?: ConstructorResponse;
 }
 
-const ConstructorImage: React.FC<ConstructorImageProps> = ({ constructor }) => {
+const ConstructorImage: React.FC<ConstructorImageProps> = ({ team }) => {
 
-    const { imageData, error, isLoading } = useImage({ title: constructor?.name });
+    const { imageData, error, isLoading } = useImage({ title: team?.name });
     const imageId = imageData?.pageids[0];
 
     if (isLoading) {
@@ -31,15 +31,13 @@ const ConstructorImage: React.FC<ConstructorImageProps> = ({ constructor }) => {
     return (
         <div className='w-full relative overflow-hidden rounded-xl grayscale group-hover:grayscale-0'>
             {
-
-                    <Image
-                        alt={constructor?.name}
-                        src={imageData?.pages[imageId].original ? imageData?.pages[imageId].original?.source : logo}
-                        width={imageData?.pages[imageId].original ? imageData?.pages[imageId].original.width : 0}
-                        height={imageData?.pages[imageId].original ? imageData?.pages[imageId].original.height : 0}
-                        className='object-cover h-full w-full'
-                    />
-                
+                <Image
+                    alt={constructor?.name}
+                    src={imageData?.pages[imageId].original ? imageData?.pages[imageId].original?.source : logo}
+                    width={imageData?.pages[imageId].original ? imageData?.pages[imageId].original.width : 0}
+                    height={imageData?.pages[imageId].original ? imageData?.pages[imageId].original.height : 0}
+                    className='object-cover h-full w-full'
+                />
             }
         </div>
     );
