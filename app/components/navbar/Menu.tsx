8f3a3df46ade49fc95f149d3db011f44678381b9
JoseAl05 <jparancibialinker@gmail.com>
2023-06-21 13:27:56 -0400

    add new param to circuits pathname

--- app/components/navbar/Menu.tsx
+++ app/components/navbar/Menu.tsx
@@ -4,7 +4,7 @@ const Menu = () => {
     return (
         <>
             <Link href='/' className='block md:hidden lg:hidden font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Inicio</Link>
-            <Link href='/circuits?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Circuitos</Link>
+            <Link href='/circuits?country=all&page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Circuitos</Link>
             <Link href='/seasons?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Temporadas</Link>
             <Link href='/drivers?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Pilotos</Link>
             <Link href='/constructors?page=0' className='font-semibold text-black text-xl cursor-pointer transition hover:text-[#D72323]'>Constructores</Link>
